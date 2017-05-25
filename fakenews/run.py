4c6bda465f97a0cd174bb7d0a125fcc3d2264868
xwhan <xwhan@cs.ucsb.edu>
2017-05-25 15:46:35 -0700

    Text classifier added

--- fakenews/run.py
+++ fakenews/run.py
@@ -3,35 +3,54 @@
 # import the required packages here
 import gensim
 import tensorflow as tf
-from tensorflow.contrib import learn
+from tensorflow.contrib import learn, rnn
 from vocab import *
 
-num_epochs = 50
+num_epochs = 20
 evaluate_step = 100
 num_filters = 128
 batch_size = 64
 
-class CNNClassifier(object):
-	"""docstring for CNNClassifier"""
-	def __init__(self, sequence_length, num_classes, filter_sizes, num_filers, embedding_matrix, l2_reg_lambda=0.0):
-		super(CNNClassifier, self).__init__()		
-		self.embedding_size = embedding_matrix.shape[1]
-		
-		self.input = tf.placeholder(tf.int32, [None, sequence_length], name='input_X')
-		self.label = tf.placeholder(tf.float32, [None, num_classes], name='input_Y')
+class Classifier(object):
+	"""docstring for Classifier"""
+	def __init__(self, num_layers, max_seq_len, num_classes, filter_sizes, num_filers, embedding_matrix, n_hidden, l2_reg_lambda=0.0):
+		super(Classifier, self).__init__()
+
+		### input data
+		self.input_seq = tf.placeholder(tf.int32, [None,max_seq_len], name='input_seq')
+		self.seq_len = tf.placeholder(tf.int32, [None], name='seq_length')
+
+		self.input_meta = tf.placeholder(tf.float32, [None, 6, 300], name='input_meta') # Encode the metadata as a matrix
+		self.label = tf.placeholder(tf.int32, [None, num_classes], name='input_y')
+
 		self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+		with tf.device('/cpu:0'),tf.variable_scope('embedding'):
+			self.W = tf.Variable(embedding_matrix,name='W',dtype=tf.float32)
+			self.lstm_input = tf.nn.embedding_lookup(self.W, self.input_seq)
+
+		self.embedding_layer_ = tf.expand_dims(self.input_meta,-1)
+
+		### Multi-layer LSTM for statement
+		cells = []
+		for _ in range(num_layers):
+			cells.append(tf.contrib.rnn.BasicLSTMCell(n_hidden))
+
+		self.cell = rnn.MultiRNNCell(cells)
+		self.lstm_input = tf.unstack(self.lstm_input, max_seq_len, 1)
+		lstm_outputs,_ = tf.contrib.rnn.static_rnn(self.cell, self.lstm_input, sequence_length=self.seq_len, dtype=tf.float32)
+		lstm_outputs = tf.transpose(tf.stack(lstm_outputs), [1,0,2])
+		batch_size = tf.shape(lstm_outputs)[0]
+		index = tf.range(0,batch_size) * max_seq_len + (self.seq_len - 1)
+		self.lstm_outputs = tf.gather(tf.reshape(lstm_outputs, [-1, n_hidden]), index)
 
 		l2_loss = tf.constant(0.0)
 
-		with tf.device('/cpu:0'),tf.variable_scope('embedding'):
-			self.W = tf.Variable(embedding_matrix,name='W',dtype=tf.float32) 
-			self.embedding_layer = tf.nn.embedding_lookup(self.W,self.input)
-			self.embedding_layer_ = tf.expand_dims(self.embedding_layer,-1)
-
+		### CNN for meta data
 		pooling = []
 		for i, filter_size in enumerate(filter_sizes):
 			with tf.variable_scope("conv-maxpool-%s" % filter_size):
-				filter_shape = [filter_size, self.embedding_size, 1, num_filters]
+				filter_shape = [filter_size, embedding_matrix.shape[1], 1, num_filters]
 				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
 				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
 				conv = tf.nn.conv2d(
@@ -43,7 +62,7 @@ class CNNClassifier(object):
 				h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
 				pooled = tf.nn.max_pool(
 					h,
-					ksize=[1, sequence_length - filter_size + 1, 1, 1],
+					ksize=[1, 6 - filter_size + 1, 1, 1],
 					strides=[1, 1, 1, 1],
 					padding='VALID',
 					name="pool")
@@ -53,11 +72,14 @@ class CNNClassifier(object):
 		self.h_pool = tf.concat(pooling, 3)
 		self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
+		### Concatenate CNN and LSTM features
+		self.features = tf.concat([self.lstm_outputs, self.h_pool_flat], axis=1)
+
 		with tf.variable_scope('dropout'):
-			self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+			self.h_drop = tf.nn.dropout(self.features, self.dropout_keep_prob)
 
 		with tf.variable_scope('output'):
-			W = tf.Variable(tf.truncated_normal([num_filters_total, num_classes], stddev=0.1), name="W")
+			W = tf.Variable(tf.truncated_normal([num_filters_total + n_hidden, num_classes], stddev=0.1), name="W")
 			b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
 			l2_loss += tf.nn.l2_loss(W)
 			l2_loss += tf.nn.l2_loss(b)
@@ -70,7 +92,7 @@ class CNNClassifier(object):
 
 		with tf.variable_scope('optimize'):
 			self.global_step = tf.Variable(0, name="global_step", trainable=False)
-			optimizer = tf.train.AdamOptimizer(1e-3)
+			optimizer = tf.train.AdamOptimizer(1e-4)
 			grads_and_vars = optimizer.compute_gradients(self.loss)
 			self.train_op = optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)
 
@@ -78,20 +100,31 @@ class CNNClassifier(object):
 			correct_predictions = tf.equal(self.predictions, tf.argmax(self.label, 1))
 			self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
 
-	def update_step(self,data_x,data_y,dropout,sess=None):
+	def update_step(self,data_x, data_meta, data_lens, data_y,dropout,sess=None):
 		sess = sess or tf.get_default_session()
-		_, step, loss, accuracy = sess.run([self.train_op,self.global_step, self.loss, self.accuracy], {self.input:data_x, self.label:data_y,self.dropout_keep_prob:dropout})
+		_, step, loss, accuracy = sess.run([self.train_op,self.global_step, self.loss, self.accuracy], {self.input_seq:data_x, self.input_meta:data_meta, self.seq_len:data_lens ,self.label:data_y,self.dropout_keep_prob:dropout})
 		return [step, loss, accuracy]
 
-	def predict(self,data_x, data_y,sess=None):
+	def predict(self,data_x, data_meta, data_lens, data_y,sess=None):
 		sess = sess or tf.get_default_session()
-		loss, accuracy, predictions = sess.run([self.loss, self.accuracy, self.predictions],{self.input:data_x, self.label:data_y, self.dropout_keep_prob:1.0})
+		loss, accuracy, predictions = sess.run([self.loss, self.accuracy, self.predictions],{self.input_seq:data_x, self.input_meta:data_meta, self.seq_len:data_lens ,self.label:data_y, self.dropout_keep_prob:1.0})
 		return [loss, accuracy, predictions]
+
+	def test(self, data_x, data_meta, data_lens, output_file, sess=None):
+		sess = sess or tf.get_default_session()
+		predictions = sess.run(self.predictions, {self.input_seq:data_x, self.input_meta:data_meta, self.seq_len:data_lens, self.dropout_keep_prob:1.0})
+		label_dict = {0:'pants-fire',1:'false',2:'barely-true',3:'half-true',4:'mostly-true',5:'true'}
+		g = open(output_file,'w')
+		for item in predictions:
+			g.write(label_dict[item] + '\n')
+		g.close()
 
 def run(train_file, valid_file, test_file, output_file):
 	print 'Preparing train/dev data'
-	x_train, y_train = build_data(train_file, 79)
-	x_dev, y_dev = build_data(valid_file, 79)
+	model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin',binary = True)
+	x_train, x_train_meta, x_train_lens, y_train = build_data(train_file, 79, model)
+	x_dev, x_dev_meta, x_dev_lens, y_dev = build_data(valid_file, 79, model)
+	x_test, x_test_meta, x_test_lens = build_data(test_file, 79, model,test=True)
 
 	print 'Loading word2vec embeddings'
 	embedding = np.load('embeddings.npy')
@@ -100,19 +133,22 @@ def run(train_file, valid_file, test_file, output_file):
 		conf = tf.ConfigProto(allow_soft_placement=True)
 		sess = tf.Session(config = conf)
 		with sess.as_default():
-			classifier = CNNClassifier(x_train.shape[1], 6, [3,4,5], 128, embedding, l2_reg_lambda=0.0)
+			classifier = Classifier(1, 79, 6, [5,3,4], 64, embedding, 300, l2_reg_lambda=0.3)
 			sess.run(tf.global_variables_initializer())
 			for epoch in range(num_epochs):
-				batches = batch_generator(x_train,y_train, batch_size)
+				batches = batch_generator(x_train,x_train_meta, x_train_lens,y_train, batch_size, shuffle=True)
 				for batch in batches:
-					current_step, current_loss, batch_accuracy = classifier.update_step(batch[0], batch[1], 0.5, sess)
+					current_step, current_loss, batch_accuracy = classifier.update_step(batch[0], batch[1], batch[2], batch[3], 0.5, sess)
 					# if current_step % evaluate_step == 0:
 					# 	_1, dev_accuracy, _2 = classifier.predict(x_dev, y_dev, sess)
 					# 	print '\nACCURACY ON DEV SET: %f' % dev_accuracy
-				loss, accuracy, predictions = classifier.predict(x_train, y_train)
+				loss, accuracy, predictions = classifier.predict(x_train, x_train_meta, x_train_lens, y_train)
 				print '\nTRAINNING ERROR & ACCURACY AFTER %d EPOCH: %f, %f' % (epoch, loss, accuracy)
-				loss, accuracy, predictions = classifier.predict(x_dev, y_dev)
+				loss, accuracy, predictions = classifier.predict(x_dev, x_dev_meta, x_dev_lens, y_dev)
 				print 'VALIDATION ERROR & ACCURACY AFTER %d EPOCH: %f, %f' % (epoch, loss, accuracy)
+				print 'SAVEING RESULTS'
+				classifier.test(x_test, x_test_meta, x_test_lens, str(epoch) + output_file)
+
 
 if __name__ == '__main__':
-	run('liar_dataset/train.tsv', 'liar_dataset/valid.tsv', 'liar_dataset/valid.tsv', 'liar_dataset/predictions.txt')
+	run('liar_dataset/train.tsv', 'liar_dataset/valid.tsv', 'liar_dataset/test.tsv', 'predictions.txt')
--- fakenews/vocab.py
+++ fakenews/vocab.py
@@ -49,54 +49,108 @@ def build_vocab():
 
 def build_embedding():
 	model = gensim.models.Word2Vec.load_word2vec_format('GoogleNews-vectors-negative300.bin',binary = True)
-	train_data = open('liar_dataset/valid.tsv').readlines()
-	x_text = []
-	for sample in train_data:
-		sentence = clean_str(sample.split('\t')[1][:-1])
-		x_text.append(sentence)
-	max_length = max([len(x.split()) for x in x_text])
-	print max_length
-	vocab_processor = learn.preprocessing.VocabularyProcessor(max_length)
-	x = np.array(list(vocab_processor.fit_transform(x_text)))
-	word2id = vocab_processor.vocabulary_._mapping
-	sorted_vocab = sorted(word2id.items(), key = lambda x:x[1])
-	embeddings = []
-	for (word,id_) in sorted_vocab:
-		try:
-			embeddings.append(model.wv[word])
-		except Exception as e:
-			embeddings.append(np.random.rand(300) - 0.5)
-	np.save('embeddings.npy', embeddings)
+	print model.wv[clean_str('foreign-policy')]
+	# train_data = open('liar_dataset/valid.tsv').readlines()
+	# x_text = []
+	# for sample in train_data:
+	# 	sentence = clean_str(sample.split('\t')[1][:-1])
+	# 	x_text.append(sentence)
+	# max_length = max([len(x.split()) for x in x_text])
+	# print max_length
+	# vocab_processor = learn.preprocessing.VocabularyProcessor(max_length)
+	# x = np.array(list(vocab_processor.fit_transform(x_text)))
+	# word2id = vocab_processor.vocabulary_._mapping
+	# sorted_vocab = sorted(word2id.items(), key = lambda x:x[1])
+	# embeddings = []
+	# for (word,id_) in sorted_vocab:
+	# 	try:
+	# 		embeddings.append(model.wv[word])
+	# 	except Exception as e:
+	# 		embeddings.append(np.random.rand(300) - 0.5)
+	# np.save('embeddings.npy', embeddings)
 
-def build_data(data_path, max_length):
+def meta2vec(model, words):
+	vectors = []
+	for word in words:
+		try:
+			vectors.append(model.wv[word])
+		except Exception as e:
+			vectors.append(np.random.rand(300) - 0.5)
+	if len(vectors) == 0:
+		vectors.append(np.zeros(300))
+	return np.sum(vectors,0).reshape((1,-1))
+
+def build_data(data_path, max_length, model, test=False):
+	if test == True:
+		data = open(data_path).readlines()
+		x_text = []
+		x_meta = []
+		x_lens = []
+		for sample in data:
+			sentence = clean_str(sample.split('\t')[0][:-1])
+			subjects = meta2vec(model,clean_str(sample.split('\t')[1]).split())
+			speaker = meta2vec(model,clean_str(sample.split('\t')[2]).split())
+			job = meta2vec(model,clean_str(sample.split('\t')[3]).split())
+			state = meta2vec(model,clean_str(sample.split('\t')[4]).split())
+			party = meta2vec(model,clean_str(sample.split('\t')[5]).split())
+			context = meta2vec(model,clean_str(sample.split('\t')[6]).split())
+			meta = np.concatenate((subjects, speaker,job,state,party,context), axis=0)
+			x_meta.append(meta)
+			x_text.append(sentence)
+			x_lens.append(len(sentence.split()))
+		vocab_processor = learn.preprocessing.VocabularyProcessor(max_length)
+		x = np.array(list(vocab_processor.fit_transform(x_text)))
+		x_meta = np.array(x_meta)
+		x_lens = np.array(x_lens)
+		return [x, x_meta, x_lens]
+
 	label_dict = {'pants-fire':0,'false':1,'barely-true':2,'half-true':3,'mostly-true':4,'true':5}
 	data = open(data_path).readlines()
 	x_text = []
 	labels = []
+	x_meta = []
+	x_lens = []
 	for sample in data:
 		labels.append(label_dict[sample.split('\t')[0]])
 		sentence = clean_str(sample.split('\t')[1][:-1])
+		subjects = meta2vec(model,clean_str(sample.split('\t')[2]).split())
+		speaker = meta2vec(model,clean_str(sample.split('\t')[3]).split())
+		job = meta2vec(model,clean_str(sample.split('\t')[4]).split())
+		state = meta2vec(model,clean_str(sample.split('\t')[5]).split())
+		party = meta2vec(model,clean_str(sample.split('\t')[6]).split())
+		context = meta2vec(model,clean_str(sample.split('\t')[7]).split())
+		meta = np.concatenate((subjects, speaker,job,state,party,context), axis=0)
+		x_meta.append(meta)
 		x_text.append(sentence)
+		x_lens.append(len(sentence.split()))
+
 	labels_onehot = np.zeros((len(labels),6))
 	labels_onehot[np.arange(len(labels)),np.array(labels)] = 1
 	vocab_processor = learn.preprocessing.VocabularyProcessor(max_length)
 	x = np.array(list(vocab_processor.fit_transform(x_text)))
-	return [x, labels_onehot]
+	x_meta = np.array(x_meta)
+	x_lens = np.array(x_lens)
+	return [x, x_meta, x_lens, labels_onehot]
 
-def batch_generator(data_x, data_y, batch_size, shuffle=True):
+def batch_generator(data_x, data_x_meta, data_lens, data_y, batch_size, shuffle=True):
 	data_size = data_x.shape[0]
 	num_batches = int((data_size - 1)/batch_size) + 1
 	if shuffle:
 		shuffle_indices = np.random.permutation(np.arange(data_size))
 		data_x_shuffle = data_x[shuffle_indices]
+		data_x_meta_shuffle = data_x_meta[shuffle_indices]
 		data_y_shuffle = data_y[shuffle_indices]
+		data_lens_shuffle = data_lens[shuffle_indices]
 	else:
 		data_x_shuffle = data_x
 		data_y_shuffle = data_y
+		data_x_meta_shuffle = data_x_meta
+		data_lens_shuffle = data_lens
 	for batch in range(num_batches):
 		start_index = batch * batch_size
 		end_index = min((batch + 1) * batch_size, data_size)
-		yield [data_x_shuffle[start_index:end_index],data_y_shuffle[start_index:end_index]]
+		yield [data_x_shuffle[start_index:end_index],data_x_meta_shuffle[start_index:end_index], data_lens_shuffle[start_index:end_index],data_y_shuffle[start_index:end_index]]
 
 if __name__ == '__main__':
-	build_embedding()
+	#build_embedding()
+	build_data('liar_dataset/valid.tsv', 79)
